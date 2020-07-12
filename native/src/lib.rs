214054ff86d6f6fd6c02f6c6c06ba51dc28a1b30
Lukas Bombach <lukas.bombach@googlemail.com>
2020-07-12 10:17:08 +0200

    created method to open a window

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,9 +1,48 @@
-use neon::prelude::*;
+#[macro_use]
+extern crate neon;
+extern crate sdl2;
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node"))
+use neon::context::Context;
+use neon::types::JsUndefined;
+
+pub struct Canvas();
+
+declare_types! {
+    pub class JsCanvas for Canvas {
+        init(mut _cx) {
+            Ok(Canvas())
+        }
+
+        method open(mut cx) {
+            let sdl_context = sdl2::init()
+            .or_else(|err| cx.throw_error(err.to_string()))?;
+
+            let video_subsystem = sdl_context.video()
+                .or_else(|err| cx.throw_error(err.to_string()))?;
+
+            let _window = video_subsystem
+                .window("Game", 900, 700)
+                .resizable()
+                .build()
+                .or_else(|err| cx.throw_error(err.to_string()))?;
+
+            let mut event_pump = sdl_context.event_pump().or_else(|err| cx.throw_error(err.to_string()))?;
+            'main: loop {
+                for event in event_pump.poll_iter() {
+                    match event {
+                        sdl2::event::Event::Quit { .. } => break 'main,
+                        _ => {}
+                    }
+                }
+                // render window contents here
+            }
+            Ok(JsUndefined::new().upcast())
+        }
+    }
 }
 
 register_module!(mut cx, {
-    cx.export_function("hello", hello)
+    cx.export_class::<JsCanvas>("Canvas")?;
+
+    Ok(())
 });
